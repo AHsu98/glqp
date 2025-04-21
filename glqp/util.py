959084413151df3d322424461ae435fcbf405666
AHsu98 <ahsu20@students.claremontmckenna.edu>
2025-04-21 14:24:37 -0700

    Perform first step of iterative refinement more often

--- glqp/util.py
+++ glqp/util.py
@@ -35,6 +35,7 @@ def factor_and_solve(
             sol = solver.solve(rhs)
             if np.any(np.isnan(sol)):
                 raise ValueError(f"NaNs found in solution to linear system with tau = {tau_reg}")
+            #Check if linear solve is terrible
             res = rhs - G@sol
             if norm2(res)>0.95*norm2(rhs):
                 raise ValueError(
@@ -42,7 +43,9 @@ def factor_and_solve(
                     f{np.sqrt(norm2(res)/norm2(rhs))}
                     """
                     )
-            num_refine = 0
+            sol = sol + solver.solve(res)
+            res = rhs - G@sol
+            num_refine = 1
             for i in range(max_refinement_steps):
                 if maxnorm(res)>=target_atol:
                     sol = sol + solver.solve(res)
